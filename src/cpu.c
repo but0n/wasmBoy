002e5a0daa49cc668c288d40613a352cb65a4dda
but0n <but0n@icloud.com>
2019-07-04 18:14:46 +0800

    feat byte rotate, byte shift, bit set & reset

--- src/cpu.c
+++ src/cpu.c
@@ -4,7 +4,7 @@
 
 static void sub_A_B();
 static void add_A_B();
-
+static void rlca();
 
 #define REG_AMOUNT 6
 
@@ -95,7 +95,7 @@ void debug() {
 int main()
 {
     F = 0xF;
-    A = 20;
+    A = 0x94;
     B = 10;
     C = 0xC;
     D = 0xD;
@@ -105,7 +105,7 @@ int main()
     SP = 0x0F;
     PC = 0xAAF0;
     debug();
-    sub_A_B();
+    rlca();
     debug();
     return 0;
 }
@@ -117,27 +117,20 @@ static unsigned char ct;
 
 void (*CB[0x100])() = {};
 
-static void NOP() {
-    ft = 4;
-}
-
-void (*op_map[])() = {
-    NOP
-};
-
 unsigned char ROM[0xFF];
 
-void exe() {
-    op_map[ROM[PC++]]();
-    ct += ft;
-    ft = 0;
-}
+
 
 
 #define _d8     (ROM[PC++])
 #define _d16    (ROM[PC++]|ROM[PC++]<<8)
 #define _a8     (ROM[0xFF00|_d8])
 #define _a16    (ROM[_d16])
+
+static void XX() {
+    const unsigned short loc = PC-1;
+    printf("Unimplemented instruction at $%04X\n", PC);
+}
 
 // NOTE: 8-Bit Loads
 // ld r, n
@@ -264,8 +257,13 @@ static void ld_A_a8() {A = _a8;ft = 12;}
 
 
 // NOTE: 16-Bit Loads
+
 // LD n, nn
+// n = BC,DE,HL,SP, nn = 16 bit immediate value
 static void ld_BC_d16() {BC = _d16;ft = 12;}
+static void ld_DE_d16() {DE = _d16;ft = 12;}
+static void ld_HL_d16() {HL = _d16;ft = 12;}
+static void ld_SP_d16() {SP = _d16;ft = 12;}
 
 // LD SP, HL
 static void ld_SP_HL() {SP = HL;ft = 8;}
@@ -536,6 +534,405 @@ static void swap_H() {swap=H&0xF0;H=H<<4|swap>>4;OR_FLAG(H);ft = 8;}
 static void swap_L() {swap=L&0xF0;L=L<<4|swap>>4;OR_FLAG(L);ft = 8;}
 static void swap_HL() {swap=ROM[HL]&0xF0;ROM[HL]=ROM[HL]<<4|swap>>4;OR_FLAG(ROM[HL]);ft = 16;}
 
-// DAA
+// DAA TODO:
+
+// CPL
+// Complement A register. (Flip all bits.)
+// Flags affected: - 1 1 -
+static void cpl() {A=~A;F|=0x60;ft = 4;}
+
+// CCF
+// Complement carry flag
+static void ccf() {F&=~0x60;F^=0x10;ft = 4;}
+
+// SCF
+// Set Carry flag
+static void scf() {F&=~0x60;F|=0x10;ft = 4;}
+
+// NOP
+// No operation
+static void nop() {ft = 4;}
+
+// HALT
+// Power down CPU until an interrupt occurs. Use this when ever possible to reduce energy consumption
+static unsigned char isHalt = 0;
+static void halt() {isHalt=1;ft = 4;}
+
+// STOP
+// Halt CPU & LCD display until button pressed
+static unsigned char isStop = 0;
+static void stop() {isStop=1;ft = 4;}
+
+// DI
+// This instruction disables interrupts but not immediately. Interrupts are disabled after instruction after DI is executed
+static unsigned ime = 0;
+static void di() {ime=0;ft = 4;}
+// EI
+// Enable interrupts. This intruction enables interrupts but not immediately. Interrupts are enabled after instruction after EI is executed.
+static void ei() {ime=1;ft = 4;}
 
 
+// NOTE: Rotates & Shifts
+// RLCA
+// Rotate A left. Old bit 7 to Carry flag
+// FIXME: conflict 0 0 0 C or Z 0 0 C
+#define RLCA_FLAG(n) do {\
+    F = 0;\
+    F |= (n & 1)<<F_C_BIT;\
+    Flag_zero(n);\
+} while(0)
+static void rlca() {swap=A&0x80;A=(A<<1)|(swap>>7);RLCA_FLAG(A);ft = 4;}
+
+// RLA
+// Rotate A left through Carry flag
+static void rla() {swap=A&0x80;A=(A<<1)|(F&0x10>>4);OR_FLAG(A);F|=swap>>3;ft = 4;}
+
+// RRCA
+// Rotate A right. Old bit 0 to Carry flag
+#define RRCA_FLAG(n) do {\
+    F = 0;\
+    F |= (n & 0x80)>>3;\
+    Flag_zero(n);\
+} while(0)
+static void rrca() {swap=A&1;A=(A>>1)|(swap<<7);RRCA_FLAG(A);ft = 4;}
+
+// RRA
+// Rotate A right through Carry flag
+static void rra() {swap=A&1;A=(A>>1)|(F&0x10<<3);OR_FLAG(A);F|=swap<<F_C_BIT;ft = 4;}
+
+//_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=
+// RLC n
+// Rotate n left through Carry flag; (n = A,B,C,D,E,H,L,(HL))
+static void rlc_A() {swap=A&0x80;A=(A<<1)|(swap>>7);RLCA_FLAG(A);ft = 8;}
+static void rlc_B() {swap=B&0x80;B=(B<<1)|(swap>>7);RLCA_FLAG(B);ft = 8;}
+static void rlc_C() {swap=C&0x80;C=(C<<1)|(swap>>7);RLCA_FLAG(C);ft = 8;}
+static void rlc_D() {swap=D&0x80;D=(D<<1)|(swap>>7);RLCA_FLAG(D);ft = 8;}
+static void rlc_E() {swap=E&0x80;E=(E<<1)|(swap>>7);RLCA_FLAG(E);ft = 8;}
+static void rlc_H() {swap=H&0x80;H=(H<<1)|(swap>>7);RLCA_FLAG(H);ft = 8;}
+static void rlc_L() {swap=L&0x80;L=(L<<1)|(swap>>7);RLCA_FLAG(L);ft = 8;}
+static void rlc_HL() {swap=ROM[HL]&0x80;ROM[HL]=(ROM[HL]<<1)|(swap>>7);RLCA_FLAG(ROM[HL]);ft = 16;}
+
+// RL n
+// Rotate n left through Carry flag; (n = A,B,C,D,E,H,L,(HL))
+static void rl_A() {swap=A&0x80;A=(A<<1)|(F&0x10>>4);OR_FLAG(A);F|=swap>>3;ft = 4;}
+static void rl_B() {swap=B&0x80;B=(B<<1)|(F&0x10>>4);OR_FLAG(B);F|=swap>>3;ft = 4;}
+static void rl_C() {swap=C&0x80;C=(C<<1)|(F&0x10>>4);OR_FLAG(C);F|=swap>>3;ft = 4;}
+static void rl_D() {swap=D&0x80;D=(D<<1)|(F&0x10>>4);OR_FLAG(D);F|=swap>>3;ft = 4;}
+static void rl_E() {swap=E&0x80;E=(E<<1)|(F&0x10>>4);OR_FLAG(E);F|=swap>>3;ft = 4;}
+static void rl_H() {swap=H&0x80;H=(H<<1)|(F&0x10>>4);OR_FLAG(H);F|=swap>>3;ft = 4;}
+static void rl_L() {swap=L&0x80;L=(L<<1)|(F&0x10>>4);OR_FLAG(L);F|=swap>>3;ft = 4;}
+static void rl_HL() {swap=ROM[HL]&0x80;ROM[HL]=(ROM[HL]<<1)|(F&0x10>>4);OR_FLAG(ROM[HL]);F|=swap>>3;ft = 16;}
+
+// RRC n
+// Rotate n right. Old bit 0 to Carry flag
+static void rrc_A() {swap=A&1;A=(A>>1)|(swap<<7);RRCA_FLAG(A);ft = 4;}
+static void rrc_B() {swap=B&1;B=(B>>1)|(swap<<7);RRCA_FLAG(B);ft = 4;}
+static void rrc_C() {swap=C&1;C=(C>>1)|(swap<<7);RRCA_FLAG(C);ft = 4;}
+static void rrc_D() {swap=D&1;D=(D>>1)|(swap<<7);RRCA_FLAG(D);ft = 4;}
+static void rrc_E() {swap=E&1;E=(E>>1)|(swap<<7);RRCA_FLAG(E);ft = 4;}
+static void rrc_H() {swap=H&1;H=(H>>1)|(swap<<7);RRCA_FLAG(H);ft = 4;}
+static void rrc_L() {swap=L&1;L=(L>>1)|(swap<<7);RRCA_FLAG(L);ft = 4;}
+static void rrc_HL() {swap=ROM[HL]&1;ROM[HL]=(ROM[HL]>>1)|(swap<<7);RRCA_FLAG(ROM[HL]);ft = 16;}
+
+// RR n
+// Rotate n right through Carry flag
+static void rr_A() {swap=A&1;A=(A>>1)|(F&0x10<<3);OR_FLAG(A);F|=swap<<F_C_BIT;ft = 4;}
+static void rr_B() {swap=B&1;B=(B>>1)|(F&0x10<<3);OR_FLAG(B);F|=swap<<F_C_BIT;ft = 4;}
+static void rr_C() {swap=C&1;C=(C>>1)|(F&0x10<<3);OR_FLAG(C);F|=swap<<F_C_BIT;ft = 4;}
+static void rr_D() {swap=D&1;D=(D>>1)|(F&0x10<<3);OR_FLAG(D);F|=swap<<F_C_BIT;ft = 4;}
+static void rr_E() {swap=E&1;E=(E>>1)|(F&0x10<<3);OR_FLAG(E);F|=swap<<F_C_BIT;ft = 4;}
+static void rr_H() {swap=H&1;H=(H>>1)|(F&0x10<<3);OR_FLAG(H);F|=swap<<F_C_BIT;ft = 4;}
+static void rr_L() {swap=L&1;L=(L>>1)|(F&0x10<<3);OR_FLAG(L);F|=swap<<F_C_BIT;ft = 4;}
+static void rr_HL() {swap=ROM[HL]&1;ROM[HL]=(ROM[HL]>>1)|(F&0x10<<3);OR_FLAG(ROM[HL]);F|=swap<<F_C_BIT;ft = 16;}
+
+// SLA n
+// Shift n left into Carry. LSB of n set to 0; (n = A,B,C,D,E,H,L,(HL))
+static void sla_A() {swap=A;A=(A<<1);OR_FLAG(A);F|=swap&0x80>>3;ft = 8;}
+static void sla_B() {swap=B;B=(B<<1);OR_FLAG(B);F|=swap&0x80>>3;ft = 8;}
+static void sla_C() {swap=C;C=(C<<1);OR_FLAG(C);F|=swap&0x80>>3;ft = 8;}
+static void sla_D() {swap=D;D=(D<<1);OR_FLAG(D);F|=swap&0x80>>3;ft = 8;}
+static void sla_E() {swap=E;E=(E<<1);OR_FLAG(E);F|=swap&0x80>>3;ft = 8;}
+static void sla_H() {swap=H;H=(H<<1);OR_FLAG(H);F|=swap&0x80>>3;ft = 8;}
+static void sla_L() {swap=L;L=(L<<1);OR_FLAG(L);F|=swap&0x80>>3;ft = 8;}
+static void sla_HL() {swap=ROM[HL];ROM[HL]=(ROM[HL]<<1);OR_FLAG(ROM[HL]);F|=swap&0x80>>3;ft = 16;}
+
+// SRA n
+// Shift n right into Carry. MSB doesn't change
+static void sra_A() {swap=A;A=(A>>1)|(swap&0x80);OR_FLAG(A);F|=(swap&1)<<F_C_BIT;ft = 8;}
+static void sra_B() {swap=B;B=(B>>1)|(swap&0x80);OR_FLAG(B);F|=(swap&1)<<F_C_BIT;ft = 8;}
+static void sra_C() {swap=C;C=(C>>1)|(swap&0x80);OR_FLAG(C);F|=(swap&1)<<F_C_BIT;ft = 8;}
+static void sra_D() {swap=D;D=(D>>1)|(swap&0x80);OR_FLAG(D);F|=(swap&1)<<F_C_BIT;ft = 8;}
+static void sra_E() {swap=E;E=(E>>1)|(swap&0x80);OR_FLAG(E);F|=(swap&1)<<F_C_BIT;ft = 8;}
+static void sra_H() {swap=H;H=(H>>1)|(swap&0x80);OR_FLAG(H);F|=(swap&1)<<F_C_BIT;ft = 8;}
+static void sra_L() {swap=L;L=(L>>1)|(swap&0x80);OR_FLAG(L);F|=(swap&1)<<F_C_BIT;ft = 8;}
+static void sra_HL() {swap=ROM[HL];ROM[HL]=(ROM[HL]>>1)|(swap&0x80);OR_FLAG(ROM[HL]);F|=(swap&1)<<F_C_BIT;ft = 16;}
+
+// SRL n
+// Shift n right into Carry. MSB set to 0
+static void srl_A() {swap=A;A=(A>>1);OR_FLAG(A);F|=(swap&1)<<F_C_BIT;ft = 8;}
+static void srl_B() {swap=B;B=(B>>1);OR_FLAG(B);F|=(swap&1)<<F_C_BIT;ft = 8;}
+static void srl_C() {swap=C;C=(C>>1);OR_FLAG(C);F|=(swap&1)<<F_C_BIT;ft = 8;}
+static void srl_D() {swap=D;D=(D>>1);OR_FLAG(D);F|=(swap&1)<<F_C_BIT;ft = 8;}
+static void srl_E() {swap=E;E=(E>>1);OR_FLAG(E);F|=(swap&1)<<F_C_BIT;ft = 8;}
+static void srl_H() {swap=H;H=(H>>1);OR_FLAG(H);F|=(swap&1)<<F_C_BIT;ft = 8;}
+static void srl_L() {swap=L;L=(L>>1);OR_FLAG(L);F|=(swap&1)<<F_C_BIT;ft = 8;}
+static void srl_HL() {swap=ROM[HL];ROM[HL]=(ROM[HL]>>1);OR_FLAG(ROM[HL]);F|=(swap&1)<<F_C_BIT;ft = 16;}
+
+
+// BIT b,r
+// Test bit b in register r; (b = 0 - 7, r = A,B,C,D,E,H,L,(HL))
+// Flags affected:
+//  Z - Set if bit b of register r is 0.
+//  N - Reset.
+//  H - Set.
+//  C - Not affected.
+#define BIT_FLAG(b, r) do {\
+    F |= 1<<F_H_BIT;\
+    F &= ~(1<<F_N_BIT);\
+    Flag_zero(r&~(1<<b));\
+} while(0)
+
+static void bit_0_A() {BIT_FLAG(0,A);ft = 8;}
+static void bit_0_B() {BIT_FLAG(0,B);ft = 8;}
+static void bit_0_C() {BIT_FLAG(0,C);ft = 8;}
+static void bit_0_D() {BIT_FLAG(0,D);ft = 8;}
+static void bit_0_E() {BIT_FLAG(0,E);ft = 8;}
+static void bit_0_H() {BIT_FLAG(0,H);ft = 8;}
+static void bit_0_L() {BIT_FLAG(0,L);ft = 8;}
+static void bit_0_HL() {BIT_FLAG(0,ROM[HL]);ft = 16;}
+
+static void bit_1_A() {BIT_FLAG(1,A);ft = 8;}
+static void bit_1_B() {BIT_FLAG(1,B);ft = 8;}
+static void bit_1_C() {BIT_FLAG(1,C);ft = 8;}
+static void bit_1_D() {BIT_FLAG(1,D);ft = 8;}
+static void bit_1_E() {BIT_FLAG(1,E);ft = 8;}
+static void bit_1_H() {BIT_FLAG(1,H);ft = 8;}
+static void bit_1_L() {BIT_FLAG(1,L);ft = 8;}
+static void bit_1_HL() {BIT_FLAG(1,ROM[HL]);ft = 16;}
+
+static void bit_2_A() {BIT_FLAG(2,A);ft = 8;}
+static void bit_2_B() {BIT_FLAG(2,B);ft = 8;}
+static void bit_2_C() {BIT_FLAG(2,C);ft = 8;}
+static void bit_2_D() {BIT_FLAG(2,D);ft = 8;}
+static void bit_2_E() {BIT_FLAG(2,E);ft = 8;}
+static void bit_2_H() {BIT_FLAG(2,H);ft = 8;}
+static void bit_2_L() {BIT_FLAG(2,L);ft = 8;}
+static void bit_2_HL() {BIT_FLAG(2,ROM[HL]);ft = 16;}
+
+static void bit_3_A() {BIT_FLAG(3,A);ft = 8;}
+static void bit_3_B() {BIT_FLAG(3,B);ft = 8;}
+static void bit_3_C() {BIT_FLAG(3,C);ft = 8;}
+static void bit_3_D() {BIT_FLAG(3,D);ft = 8;}
+static void bit_3_E() {BIT_FLAG(3,E);ft = 8;}
+static void bit_3_H() {BIT_FLAG(3,H);ft = 8;}
+static void bit_3_L() {BIT_FLAG(3,L);ft = 8;}
+static void bit_3_HL() {BIT_FLAG(3,ROM[HL]);ft = 16;}
+
+static void bit_4_A() {BIT_FLAG(4,A);ft = 8;}
+static void bit_4_B() {BIT_FLAG(4,B);ft = 8;}
+static void bit_4_C() {BIT_FLAG(4,C);ft = 8;}
+static void bit_4_D() {BIT_FLAG(4,D);ft = 8;}
+static void bit_4_E() {BIT_FLAG(4,E);ft = 8;}
+static void bit_4_H() {BIT_FLAG(4,H);ft = 8;}
+static void bit_4_L() {BIT_FLAG(4,L);ft = 8;}
+static void bit_4_HL() {BIT_FLAG(4,ROM[HL]);ft = 16;}
+
+static void bit_5_A() {BIT_FLAG(5,A);ft = 8;}
+static void bit_5_B() {BIT_FLAG(5,B);ft = 8;}
+static void bit_5_C() {BIT_FLAG(5,C);ft = 8;}
+static void bit_5_D() {BIT_FLAG(5,D);ft = 8;}
+static void bit_5_E() {BIT_FLAG(5,E);ft = 8;}
+static void bit_5_H() {BIT_FLAG(5,H);ft = 8;}
+static void bit_5_L() {BIT_FLAG(5,L);ft = 8;}
+static void bit_5_HL() {BIT_FLAG(5,ROM[HL]);ft = 16;}
+
+static void bit_6_A() {BIT_FLAG(6,A);ft = 8;}
+static void bit_6_B() {BIT_FLAG(6,B);ft = 8;}
+static void bit_6_C() {BIT_FLAG(6,C);ft = 8;}
+static void bit_6_D() {BIT_FLAG(6,D);ft = 8;}
+static void bit_6_E() {BIT_FLAG(6,E);ft = 8;}
+static void bit_6_H() {BIT_FLAG(6,H);ft = 8;}
+static void bit_6_L() {BIT_FLAG(6,L);ft = 8;}
+static void bit_6_HL() {BIT_FLAG(6,ROM[HL]);ft = 16;}
+
+static void bit_7_A() {BIT_FLAG(7,A);ft = 8;}
+static void bit_7_B() {BIT_FLAG(7,B);ft = 8;}
+static void bit_7_C() {BIT_FLAG(7,C);ft = 8;}
+static void bit_7_D() {BIT_FLAG(7,D);ft = 8;}
+static void bit_7_E() {BIT_FLAG(7,E);ft = 8;}
+static void bit_7_H() {BIT_FLAG(7,H);ft = 8;}
+static void bit_7_L() {BIT_FLAG(7,L);ft = 8;}
+static void bit_7_HL() {BIT_FLAG(7,ROM[HL]);ft = 16;}
+
+// SET b,r
+// Set bit b in register r; (b = 0 - 7, r = A,B,C,D,E,H,L,(HL))
+#define BIT_SET(b, r) do {\
+    r |= 1<<b;\
+} while(0)
+
+static void set_0_A() {BIT_SET(0,A);ft = 8;}
+static void set_0_B() {BIT_SET(0,B);ft = 8;}
+static void set_0_C() {BIT_SET(0,C);ft = 8;}
+static void set_0_D() {BIT_SET(0,D);ft = 8;}
+static void set_0_E() {BIT_SET(0,E);ft = 8;}
+static void set_0_H() {BIT_SET(0,H);ft = 8;}
+static void set_0_L() {BIT_SET(0,L);ft = 8;}
+static void set_0_HL() {BIT_SET(0,ROM[HL]);ft = 16;}
+
+static void set_1_A() {BIT_SET(1,A);ft = 8;}
+static void set_1_B() {BIT_SET(1,B);ft = 8;}
+static void set_1_C() {BIT_SET(1,C);ft = 8;}
+static void set_1_D() {BIT_SET(1,D);ft = 8;}
+static void set_1_E() {BIT_SET(1,E);ft = 8;}
+static void set_1_H() {BIT_SET(1,H);ft = 8;}
+static void set_1_L() {BIT_SET(1,L);ft = 8;}
+static void set_1_HL() {BIT_SET(1,ROM[HL]);ft = 16;}
+
+static void set_2_A() {BIT_SET(2,A);ft = 8;}
+static void set_2_B() {BIT_SET(2,B);ft = 8;}
+static void set_2_C() {BIT_SET(2,C);ft = 8;}
+static void set_2_D() {BIT_SET(2,D);ft = 8;}
+static void set_2_E() {BIT_SET(2,E);ft = 8;}
+static void set_2_H() {BIT_SET(2,H);ft = 8;}
+static void set_2_L() {BIT_SET(2,L);ft = 8;}
+static void set_2_HL() {BIT_SET(2,ROM[HL]);ft = 16;}
+
+static void set_3_A() {BIT_SET(3,A);ft = 8;}
+static void set_3_B() {BIT_SET(3,B);ft = 8;}
+static void set_3_C() {BIT_SET(3,C);ft = 8;}
+static void set_3_D() {BIT_SET(3,D);ft = 8;}
+static void set_3_E() {BIT_SET(3,E);ft = 8;}
+static void set_3_H() {BIT_SET(3,H);ft = 8;}
+static void set_3_L() {BIT_SET(3,L);ft = 8;}
+static void set_3_HL() {BIT_SET(3,ROM[HL]);ft = 16;}
+
+static void set_4_A() {BIT_SET(4,A);ft = 8;}
+static void set_4_B() {BIT_SET(4,B);ft = 8;}
+static void set_4_C() {BIT_SET(4,C);ft = 8;}
+static void set_4_D() {BIT_SET(4,D);ft = 8;}
+static void set_4_E() {BIT_SET(4,E);ft = 8;}
+static void set_4_H() {BIT_SET(4,H);ft = 8;}
+static void set_4_L() {BIT_SET(4,L);ft = 8;}
+static void set_4_HL() {BIT_SET(4,ROM[HL]);ft = 16;}
+
+static void set_5_A() {BIT_SET(5,A);ft = 8;}
+static void set_5_B() {BIT_SET(5,B);ft = 8;}
+static void set_5_C() {BIT_SET(5,C);ft = 8;}
+static void set_5_D() {BIT_SET(5,D);ft = 8;}
+static void set_5_E() {BIT_SET(5,E);ft = 8;}
+static void set_5_H() {BIT_SET(5,H);ft = 8;}
+static void set_5_L() {BIT_SET(5,L);ft = 8;}
+static void set_5_HL() {BIT_SET(5,ROM[HL]);ft = 16;}
+
+static void set_6_A() {BIT_SET(6,A);ft = 8;}
+static void set_6_B() {BIT_SET(6,B);ft = 8;}
+static void set_6_C() {BIT_SET(6,C);ft = 8;}
+static void set_6_D() {BIT_SET(6,D);ft = 8;}
+static void set_6_E() {BIT_SET(6,E);ft = 8;}
+static void set_6_H() {BIT_SET(6,H);ft = 8;}
+static void set_6_L() {BIT_SET(6,L);ft = 8;}
+static void set_6_HL() {BIT_SET(6,ROM[HL]);ft = 16;}
+
+static void set_7_A() {BIT_SET(7,A);ft = 8;}
+static void set_7_B() {BIT_SET(7,B);ft = 8;}
+static void set_7_C() {BIT_SET(7,C);ft = 8;}
+static void set_7_D() {BIT_SET(7,D);ft = 8;}
+static void set_7_E() {BIT_SET(7,E);ft = 8;}
+static void set_7_H() {BIT_SET(7,H);ft = 8;}
+static void set_7_L() {BIT_SET(7,L);ft = 8;}
+static void set_7_HL() {BIT_SET(7,ROM[HL]);ft = 16;}
+
+// RES b,r
+// Reset bit b in register r
+#define BIT_RES(b, r) do {\
+    r &= ~(1<<b);\
+} while(0)
+
+static void res_0_A() {BIT_RES(0,A);ft = 8;}
+static void res_0_B() {BIT_RES(0,B);ft = 8;}
+static void res_0_C() {BIT_RES(0,C);ft = 8;}
+static void res_0_D() {BIT_RES(0,D);ft = 8;}
+static void res_0_E() {BIT_RES(0,E);ft = 8;}
+static void res_0_H() {BIT_RES(0,H);ft = 8;}
+static void res_0_L() {BIT_RES(0,L);ft = 8;}
+static void res_0_HL() {BIT_RES(0,ROM[HL]);ft = 16;}
+
+static void res_1_A() {BIT_RES(1,A);ft = 8;}
+static void res_1_B() {BIT_RES(1,B);ft = 8;}
+static void res_1_C() {BIT_RES(1,C);ft = 8;}
+static void res_1_D() {BIT_RES(1,D);ft = 8;}
+static void res_1_E() {BIT_RES(1,E);ft = 8;}
+static void res_1_H() {BIT_RES(1,H);ft = 8;}
+static void res_1_L() {BIT_RES(1,L);ft = 8;}
+static void res_1_HL() {BIT_RES(1,ROM[HL]);ft = 16;}
+
+static void res_2_A() {BIT_RES(2,A);ft = 8;}
+static void res_2_B() {BIT_RES(2,B);ft = 8;}
+static void res_2_C() {BIT_RES(2,C);ft = 8;}
+static void res_2_D() {BIT_RES(2,D);ft = 8;}
+static void res_2_E() {BIT_RES(2,E);ft = 8;}
+static void res_2_H() {BIT_RES(2,H);ft = 8;}
+static void res_2_L() {BIT_RES(2,L);ft = 8;}
+static void res_2_HL() {BIT_RES(2,ROM[HL]);ft = 16;}
+
+static void res_3_A() {BIT_RES(3,A);ft = 8;}
+static void res_3_B() {BIT_RES(3,B);ft = 8;}
+static void res_3_C() {BIT_RES(3,C);ft = 8;}
+static void res_3_D() {BIT_RES(3,D);ft = 8;}
+static void res_3_E() {BIT_RES(3,E);ft = 8;}
+static void res_3_H() {BIT_RES(3,H);ft = 8;}
+static void res_3_L() {BIT_RES(3,L);ft = 8;}
+static void res_3_HL() {BIT_RES(3,ROM[HL]);ft = 16;}
+
+static void res_4_A() {BIT_RES(4,A);ft = 8;}
+static void res_4_B() {BIT_RES(4,B);ft = 8;}
+static void res_4_C() {BIT_RES(4,C);ft = 8;}
+static void res_4_D() {BIT_RES(4,D);ft = 8;}
+static void res_4_E() {BIT_RES(4,E);ft = 8;}
+static void res_4_H() {BIT_RES(4,H);ft = 8;}
+static void res_4_L() {BIT_RES(4,L);ft = 8;}
+static void res_4_HL() {BIT_RES(4,ROM[HL]);ft = 16;}
+
+static void res_5_A() {BIT_RES(5,A);ft = 8;}
+static void res_5_B() {BIT_RES(5,B);ft = 8;}
+static void res_5_C() {BIT_RES(5,C);ft = 8;}
+static void res_5_D() {BIT_RES(5,D);ft = 8;}
+static void res_5_E() {BIT_RES(5,E);ft = 8;}
+static void res_5_H() {BIT_RES(5,H);ft = 8;}
+static void res_5_L() {BIT_RES(5,L);ft = 8;}
+static void res_5_HL() {BIT_RES(5,ROM[HL]);ft = 16;}
+
+static void res_6_A() {BIT_RES(6,A);ft = 8;}
+static void res_6_B() {BIT_RES(6,B);ft = 8;}
+static void res_6_C() {BIT_RES(6,C);ft = 8;}
+static void res_6_D() {BIT_RES(6,D);ft = 8;}
+static void res_6_E() {BIT_RES(6,E);ft = 8;}
+static void res_6_H() {BIT_RES(6,H);ft = 8;}
+static void res_6_L() {BIT_RES(6,L);ft = 8;}
+static void res_6_HL() {BIT_RES(6,ROM[HL]);ft = 16;}
+
+static void res_7_A() {BIT_RES(7,A);ft = 8;}
+static void res_7_B() {BIT_RES(7,B);ft = 8;}
+static void res_7_C() {BIT_RES(7,C);ft = 8;}
+static void res_7_D() {BIT_RES(7,D);ft = 8;}
+static void res_7_E() {BIT_RES(7,E);ft = 8;}
+static void res_7_H() {BIT_RES(7,H);ft = 8;}
+static void res_7_L() {BIT_RES(7,L);ft = 8;}
+static void res_7_HL() {BIT_RES(7,ROM[HL]);ft = 16;}
+
+// JP nn
+// Jump to address nn; nn = two byte immediate value. (LS byte first.)
+
+void (*op_map[])() = {
+    // 0x
+    nop, ld_BC_d16, ld_BC_A, inc_BC, inc_B, dec_B, ld_B_d8, rlca, ld_a16_SP, add_HL_BC, ld_A_BC, dec_BC, inc_C, dec_C, ld_C_d8, rrca,
+    // 1x
+    stop, ld_DE_d16, ld_DE_A, inc_DE, inc_D, dec_D, ld_D_d8, rla,
+};
+
+
+void exe() {
+    op_map[ROM[PC++]]();
+    ct += ft;
+    ft = 0;
+}
