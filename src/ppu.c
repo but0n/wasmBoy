afe3acec920e9d9501bea81a24653adf8a49a6b8
but0n <but0n@icloud.com>
2019-07-20 10:23:16 +0800

    fix

--- src/ppu.c
+++ src/ppu.c
@@ -144,7 +144,7 @@ void scanline() {
             pixelCounter++;
 
             color_bit = (tile3 >> ((7-p-u_start)<<1)) & 3 << 1;
-            color = colorLUT[(IO_Reg->BGP >> color_bit) & 3];
+            color = colorLUT[(IO_Reg->BGP >> color_bit) & 3][0];
             // Write color
             texture[liney][pixelCounter][0] = color[0];
             texture[liney][pixelCounter][1] = color[1];
